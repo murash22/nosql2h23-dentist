a45fcb54392fd2f9993856450d9bccb115e1f1f8
10023r <ashyr22@icloud.com>
2023-12-21 18:45:39 +0300

    added routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.tsx'
 import './index.css'
-import Welcome from "./Pages/Welcome/Welcome.tsx";
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Welcome />
+    <App />
   </React.StrictMode>,
 )
