3e4db48939c0f278c2e3f6f50fd73b1544b43bf6
Maksim Tishkin <maksim-902@mail.ru>
2023-12-24 15:00:34 +0300

    Updated admin page

--- frontend/src/Pages/AdminPage/AdminPage.tsx
+++ frontend/src/Pages/AdminPage/AdminPage.tsx
@@ -1,5 +1,34 @@
+import Header from "../../Widgets/Header/Header.tsx";
+import SignOutButton from "../../Components/SignOutButton/SignOutButton.tsx";
+import {useEffect, useState} from "react";
+import {getAdminById} from "../../api/requests.ts";
+import {useNavigate} from "react-router-dom";
+import {useSelector} from "react-redux";
+
+
 function AdminPage(){
-    return <h1>Admin</h1>
+    let tmp: any = {}
+    const [info, setInfo] = useState(tmp)
+    const navigate = useNavigate()
+    const id = useSelector((state: any) => state.id)
+
+    useEffect(() => {
+        getAdminById(id)
+            .then(res => {
+                setInfo(res.data)
+
+            })
+            .catch(err => {
+                console.log(err)
+                navigate("/welcome")
+            })
+    }, []);
+   return <>
+        <Header signOutButton={<SignOutButton />} />
+        <div className="px-14 font-bold min-w-fit">
+                <h3 className="text-2xl my-7">Hello, <span className="text-base1">{info.name} {info.surname}</span>!</h3>
+        </div>
+    </>
 }
 
 export default AdminPage
