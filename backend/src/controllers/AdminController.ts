97211960080765dc753e8fe7c05c4ea90acac98b
Maksim Tishkin <maksim-902@mail.ru>
2023-12-24 15:01:01 +0300

    Admin controller modified

--- backend/src/controllers/AdminController.ts
+++ backend/src/controllers/AdminController.ts
@@ -1,7 +1,17 @@
+// @ts-ignore
 import express from "express";
+import {adminService} from "../services/AdminService";
 
 export class AdminController {
-    static async getAdminInfo(req: express.Request, res: express.Response) {
+    static async getAdminById(req: express.Request, res: express.Response) {
+        try {
+            const admin = await adminService.getAdminById(req.params.id)
 
+            res.json(admin)
+        } catch (error) {
+            if (error instanceof Error) {
+                res.status(400).send(error.message)
+            }
+        }
     }
 }
